0dad70ecd09e6f8ce6823689d3bbe46b8724dea9
K0UN1 <UWU3405180574@163.com>
2026-07-20 17:35:07 +0800

    Update patch.py

--- patch.py
+++ patch.py
@@ -32,12 +32,11 @@ with open(MSG_FILE, "r") as f:
 
 # 2a. Add imports
 if "import androidx.compose.foundation.BorderStroke" not in src:
-    import_block = (
+        import_block = (
         "import androidx.compose.foundation.BorderStroke\n"
         "import androidx.compose.foundation.Image\n"
         "import androidx.compose.foundation.layout.offset\n"
         "import androidx.compose.foundation.layout.wrapContentSize\n"
-        "import androidx.compose.foundation.layout.matchParentSize\n"
         "import androidx.compose.ui.layout.ContentScale\n"
         "import androidx.compose.ui.zIndex\n"
     )
